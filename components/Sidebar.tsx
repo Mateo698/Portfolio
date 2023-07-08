205e0e5ada346a60707c0fd6c08a14fd8f2902bd
Mateo <mateora73@gmail.com>
2023-07-08 12:43:35 -0500

    Basic files

---
+++ components/Sidebar.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Sidebar.module.css'
+
+export default function Sidebar() {
+    return(
+        <div className={styles.container}>
+            <h1>sidebar</h1>
+        </div>
+    )
+}
