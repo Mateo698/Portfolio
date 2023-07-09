81cfeb705d52ff29c8f276b205ddd15fe2a7e7e8
Mateo Rada <mateora73@gmail.com>
2023-07-08 21:55:44 -0500

    Basic structure for sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,31 @@
 import styles from '../styles/Sidebar.module.css'
+import { useState } from 'react';
 
-export default function Sidebar() {
+class section {
+    title: string;
+    constructor(title: string) {
+        this.title = title;
+    }
+}
+
+interface SidebarProps {
+    sections: section[];
+}
+
+export default function Sidebar(sections: SidebarProps) {
+    const [active, setActive] = useState(0);
+    const handleClick = (index: number) => {
+        setActive(index);
+        console.log(index);
+    }
+
     return(
         <div className={styles.container}>
-            <h1>sidebar</h1>
+            {sections.sections.map((section, index) => (
+                <div key={index} className={styles.section} onClick={() => handleClick(index)} style={active === index ? {backgroundColor: "#e5e5e5"} : {}}>
+                    <h3>{section.title}</h3>
+                </div>
+            ))}
         </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,17 @@
 import Sidebar from "../components/Sidebar"
 import styles from '../styles/Home.module.css'
 
+const sections = [
+  {title: "About"},
+  {title: "Projects"},
+  {title: "Skills"},
+  {title: "Contact"}
+]
+
 export default function Home(){
   return (
     <div className={styles.container}>
-      <Sidebar />
+      <Sidebar sections={sections}/>
     </div>
   )
 }
