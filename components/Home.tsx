d20a77b4c0713d39b45d4477b236c1e2ef513603
Mateo <mateora73@gmail.com>
2023-07-17 18:03:11 -0500

    Home mobile view

--- components/Home.tsx
+++ components/Home.tsx
@@ -7,9 +7,9 @@ export default function Page() {
     return (
         <div className={styles.container}>
             <div className={styles.description}>
-                <h2 style={{ fontWeight: 'lighter', fontSize: '50px', marginBottom: -50 }}>Hi!</h2>
-                <h1 style={{ fontWeight: 'heavy', fontSize: '70px', marginBottom: -50 }}> I'm Mateo Rada</h1>
-                <h3 style={{ fontWeight: 'lighter', fontSize: '50px' }}>Software System Engineer, student at ICESI University</h3>
+                <h2 className={styles.text1}>Hi!</h2>
+                <h1 className={styles.text2}> I'm Mateo Rada</h1>
+                <h3 className={styles.text1}>Software System Engineer, student at ICESI University</h3>
                 <div className={styles.social}>
                     <div className={styles.buttonContainer}>
                         <button className={styles.socialButton} onClick={() => window.open('https://www.linkedin.com/in/mateo-rada-arias-b44503239/')}>
